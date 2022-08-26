adb8c1f93b8abcadd4cf6fd7a628a60f5c32f952
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-08-26 14:47:36 +0000

    [Add]いいね機能

---
+++ app/models/favorite.rb
@@ -0,0 +1,10 @@
+class Favorite < ApplicationRecord
+   def change
+    create_table :favorites do |t|
+      t.integer "user_id", null: false
+      t.integer "post_id", null: false
+
+      t.timestamps
+    end
+  end
+end
