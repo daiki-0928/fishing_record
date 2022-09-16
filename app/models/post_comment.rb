0b7cd2968ed2048d80c1a7fc24a4c40a412af433
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-09-16 09:01:41 +0000

    [Add]コメント機能編集

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,3 +1,10 @@
 class PostComment < ApplicationRecord
-  
+  def change
+    create_table :favorites do |t|
+      t.integer "user_id", null: false
+      t.integer "post_id", null: false
+
+      t.timestamps
+  end
+end
 end
