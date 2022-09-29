da4404245a07dc5607ececb84d0ec8b1c272dff7
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-09-29 14:21:10 +0000

    [Add]コントローラ修正

---
+++ app/controllers/public/stater_controller.rb
@@ -0,0 +1,2 @@
+class Public::StaterController < ApplicationController
+end
---
+++ test/controllers/public/stater_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Public::StaterControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
