d8c4f94423a4bf4f11043a81df3e4790420b04ee
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-10-01 14:59:05 +0000

    [Add]route編集

---
+++ app/helpers/admin/dees_helper.rb
@@ -0,0 +1,2 @@
+module Admin::DeesHelper
+end
---
+++ test/controllers/admin/dees_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Admin::DeesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
