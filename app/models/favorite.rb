1e846b5d43bf42e4b97e22b06c1cc91c109549b1
daiki-0928 <s.daikin2811@ezweb.ne.jp>
2022-10-07 14:50:20 +0000

    [Add]コントローラ修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -19,3 +19,14 @@ class Favorite < ApplicationRecord
     end
   end
 end
+
+class Favorite < ApplicationRecord
+   def change
+    create_table :favorites do |t|
+      t.integer "user_id", null: false
+      t.integer "post_id", null: false
+
+      t.timestamps
+    end
+  end
+end
